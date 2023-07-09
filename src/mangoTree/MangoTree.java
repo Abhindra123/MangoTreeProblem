8af4bbc8300ff8e096ebcdda9410e88e21f753d6
Abhindra123 <abhindraganesamoorthy@gmail.com>
2023-07-09 21:43:39 +0530

    commit update

---
+++ src/mangoTree/MangoTree.java
@@ -0,0 +1,28 @@
+package mangoTree;
+import java.io.*;
+import java.util.*;
+public class MangoTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		int rows=scanner.nextInt();
+		int columns=scanner.nextInt();
+		int tree=scanner.nextInt();
+		
+		if(tree<=rows) {
+			System.out.print(true);
+		}
+		else if((tree-1)%rows==0) {
+			System.out.print(true);
+		}
+		else if(tree%rows==0) {
+			System.out.print(true);
+		}
+		else {
+			System.out.print(false);
+		}
+
+	}
+
+}
